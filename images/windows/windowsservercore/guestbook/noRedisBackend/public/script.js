dd0e98e4a27b8c45cfbfb7a8bdab4a10e2aa5b2a
Michael Michael <michmike@cs.stanford.edu>
2017-03-31 11:40:27 -0500

    asddfdd
    xcv

--- images/windows/windowsservercore/guestbook/noRedisBackend/public/script.js
+++ images/windows/windowsservercore/guestbook/noRedisBackend/public/script.js
@@ -1,37 +1,24 @@
 $(document).ready(function() {
   var headerTitleElement = $("#header h1");
-//  var entriesElement = $("#guestbook-entries");
   var formElement = $("#envy-form");
   var submitElement = $("#envy-submit");
-//  var entryContentElement = $("#guestbook-entry-content");
   var hostAddressElement = $("#guestbook-host-address");
-/*
-  var appendGuestbookEntries = function(data) {
-    entriesElement.empty();
-    $.each(data, function(key, val) {
-      entriesElement.append("<p>" + val + "</p>");
-    });
-  }
-*/
 
   $.getJSON("env", function(result){
-        $.each(result, function(i, field){
-			var theDiv = document.getElementById("environmentDetails");
-			var content = document.createTextNode("<li>" + i + " = " + field);
-			theDiv.appendChild(content);            
-        });
-    });
+	$.each(result, function(i, field){
+		var theDiv = document.getElementById("environmentDetails");
+		theDiv.innerHTML = theDiv.innerHTML + "<li>" + i + " = " + field;
+		//var content = document.createTextNode();
+		//theDiv.appendChild(content);            
+	});
+  });
 
   var donothing = function(data) {
   }
 
   var handleSubmission = function(e) {
     e.preventDefault();	
-    //var entryValue = entryContentElement.val()
-    //if (entryValue.length > 0) {
-//      entriesElement.append("<p>...</p>");
-      $.getJSON("rpush/envy/crashme", donothing);
-  //  }
+    $.getJSON("rpush/envy/crashme", donothing);
     return false;
   }
 
@@ -40,19 +27,10 @@ $(document).ready(function() {
   var randomColor = colors[Math.floor(5 * Math.random())];
   (function setElementsColor(color) {
     headerTitleElement.css("color", color);
-    //entryContentElement.css("box-shadow", "inset 0 0 0 2px " + color);
     submitElement.css("background-color", color);
   })(randomColor);
 
   submitElement.click(handleSubmission);
   formElement.submit(handleSubmission);
   hostAddressElement.append(document.URL);
-
-  // Poll every second.
-//  (function fetchGuestbook() {
-//    $.getJSON("lrange/guestbook").done(appendGuestbookEntries).always(
-  //    function() {
-//        setTimeout(fetchGuestbook, 1000);
-      //});
-  //})();
 });
