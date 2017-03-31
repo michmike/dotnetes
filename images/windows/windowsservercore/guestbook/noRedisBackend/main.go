6544f51a64095115a38f961f0d06af14fa22d513
Michael Michael <michmike@cs.stanford.edu>
2017-03-31 09:23:36 -0500

    changes

--- images/windows/windowsservercore/guestbook/noRedisBackend/main.go
+++ images/windows/windowsservercore/guestbook/noRedisBackend/main.go
@@ -1,19 +1,3 @@
-/*
-Copyright 2014 The Kubernetes Authors.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package main
 
 import (
@@ -27,30 +11,10 @@ import (
 	"github.com/xyproto/simpleredis"
 )
 
-var (
-	masterPool *simpleredis.ConnectionPool
-	slavePool  *simpleredis.ConnectionPool
-)
-
-func ListRangeHandler(rw http.ResponseWriter, req *http.Request) {
-	key := mux.Vars(req)["key"]
-	list := simpleredis.NewList(slavePool, key)
-	members := HandleError(list.GetAll()).([]string)
-	membersJSON := HandleError(json.MarshalIndent(members, "", "  ")).([]byte)
-	rw.Write(membersJSON)
-}
-
 func ListPushHandler(rw http.ResponseWriter, req *http.Request) {
 	key := mux.Vars(req)["key"]
 	value := mux.Vars(req)["value"]
-	list := simpleredis.NewList(masterPool, key)
-	HandleError(nil, list.Add(value))
-	ListRangeHandler(rw, req)
-}
-
-func InfoHandler(rw http.ResponseWriter, req *http.Request) {
-	info := HandleError(masterPool.Get(0).Do("INFO")).([]byte)
-	rw.Write(info)
+		
 }
 
 func EnvHandler(rw http.ResponseWriter, req *http.Request) {
@@ -74,13 +38,7 @@ func HandleError(result interface{}, err error) (r interface{}) {
 }
 
 func main() {
-	masterPool = simpleredis.NewConnectionPoolHost("redis-master:6379")
-	defer masterPool.Close()
-	slavePool = simpleredis.NewConnectionPoolHost("redis-slave:6379")
-	defer slavePool.Close()
-
 	r := mux.NewRouter()
-	r.Path("/lrange/{key}").Methods("GET").HandlerFunc(ListRangeHandler)
 	r.Path("/rpush/{key}/{value}").Methods("GET").HandlerFunc(ListPushHandler)	
 	r.Path("/env").Methods("GET").HandlerFunc(EnvHandler)
 
--- images/windows/windowsservercore/guestbook/noRedisBackend/public/script.js
+++ images/windows/windowsservercore/guestbook/noRedisBackend/public/script.js
@@ -1,25 +1,29 @@
 $(document).ready(function() {
   var headerTitleElement = $("#header h1");
-  var entriesElement = $("#guestbook-entries");
-  var formElement = $("#guestbook-form");
-  var submitElement = $("#guestbook-submit");
-  var entryContentElement = $("#guestbook-entry-content");
+//  var entriesElement = $("#guestbook-entries");
+  var formElement = $("#form");
+  var submitElement = $("#submit");
+//  var entryContentElement = $("#guestbook-entry-content");
   var hostAddressElement = $("#guestbook-host-address");
-
+/*
   var appendGuestbookEntries = function(data) {
     entriesElement.empty();
     $.each(data, function(key, val) {
       entriesElement.append("<p>" + val + "</p>");
     });
   }
+*/
+
+  var donothing = function(data) {
+  }
 
   var handleSubmission = function(e) {
     e.preventDefault();
-    var entryValue = entryContentElement.val()
-    if (entryValue.length > 0) {
-      entriesElement.append("<p>...</p>");
-  //    $.getJSON("rpush/guestbook/" + entryValue, appendGuestbookEntries);
-    }
+    //var entryValue = entryContentElement.val()
+    //if (entryValue.length > 0) {
+//      entriesElement.append("<p>...</p>");
+      $.getJSON("rpush/guestbook/crashme", donothing);
+  //  }
     return false;
   }
 
